f9f0f2e3c40ef044c006a32d023759fb09adc1a7
Avion <124367706+AvioNHarM@users.noreply.github.com>
2025-05-30 18:25:54 +0300

    Backend/DB/added the database models

--- Backend/DrugStore/admin.py
+++ Backend/DrugStore/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Accounts, Products, Purchases, UserViewHistory, CartItem
 
 # Register your models here.
+admin.site.register(Accounts)
+admin.site.register(Products)
+admin.site.register(Purchases)
+admin.site.register(UserViewHistory)
+admin.site.register(CartItem)
